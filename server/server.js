4b91c4153eb4aad83c3db23c06595dbfd42a6964
Eunice Tan <eunice_tan@byu.edu>
2015-11-27 11:16:27 -0700

    Server Scripting

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// setup static directory
+app.use(express.static('./client'));
+
+
+// start the server
+var server = app.listen(3000, function() {
+console.log("Started on port 3000");
+var host = server.address().address;
+var port = server.address().port;
+
+
+});
